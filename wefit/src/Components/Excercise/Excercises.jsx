31310e5ccc8ca05fe202ec739588dd9c41ee8e48
Himanku Gogoi <himankugogoi5@gmail.com>
2022-12-14 23:29:03 +0530

    day-2 files added

---
+++ wefit/src/Components/Excercise/Excercises.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import { Box, Grid, Image, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Excercises = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = async () => {
+    let data = axios.get("http://localhost:8080/excercises");
+    let res = await data;
+    setData(res.data);
+  };
+  console.log(data);
+
+  return (
+    <Box mt="50px" textAlign="center">
+      <Text fontSize="24px" paddingTop="20px">
+        Try Something New
+      </Text>
+      <Text
+        fontSize="20px"
+        paddingTop="10px"
+        color="rgb(88, 95, 105)"
+        paddingBottom="20px"
+      >
+        Change up your routine with one of these exercises!
+      </Text>
+      <Grid
+        templateColumns={{
+          base: "repeat(2,1fr)",
+          md: "repeat(4,1fr)",
+          lg: "repeat(6,1fr)",
+        }}
+        w={{ base: "90%", md: "80%", lg: "75%" }}
+        margin="auto"
+        gap="15px"
+      >
+        {data.map((el) => (
+          <Box key={el.id} w="100%">
+            <Link to={`/dummy/${el.id}`}>
+              <Image src={el.image_urls[0]} w="100%" />
+              <Text
+                fontSize="14px"
+                margin="5px"
+                color="#0ab2e6"
+                fontWeight="500"
+              >
+                {el.name}
+              </Text>
+              <Text fontSize="14px" margin="5px">
+                {el.main_group}, {el.detailed_group}
+              </Text>
+            </Link>
+          </Box>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default Excercises;
---
+++ wefit/src/Components/Excercise/MuscleGroup.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { GridItem, Grid, Image, Box, Button, Center } from "@chakra-ui/react";
+import styles from "../../Styles/Excercise.module.css";
+
+const MuscleGroup = () => {
+  return (
+    <Box
+      position="relative"
+      w={{ base: "90vw", md: "75vw", lg: "45vw" }}
+      h={{ base: "65vw", md: "50vw", lg: "34vw" }}
+      margin="auto"
+    >
+      <Grid className={styles.muscle}>
+        <GridItem>
+          <Button size={{base:"sm", sm:"sm",md:"md",lg:"md"}}>Triceps</Button>
+        </GridItem>
+        <GridItem colSpan={2}></GridItem>
+        <GridItem>
+          <Button size={{base:"sm", sm:"sm",md:"md",lg:"md"}}>Shoulders</Button>
+        </GridItem>
+        <GridItem>
+          <Button size={{base:"sm", sm:"sm",md:"md",lg:"md"}}>Biceps</Button>
+        </GridItem>
+        <GridItem colSpan={2}></GridItem>
+        <GridItem>
+          <Button size={{base:"sm", sm:"sm",md:"md",lg:"md"}}>Chest</Button>
+        </GridItem>
+        <GridItem>
+          <Button size={{base:"sm", sm:"sm",md:"md",lg:"md"}}>Back</Button>
+        </GridItem>
+        <GridItem colSpan={2}></GridItem>
+        <GridItem>
+          <Button size={{base:"sm", sm:"sm",md:"md",lg:"md"}}>Forearms</Button>
+        </GridItem>
+        <GridItem>
+          <Button size={{base:"sm", sm:"sm",md:"md",lg:"md"}}>Glutes</Button>
+        </GridItem>
+        <GridItem colSpan={2}></GridItem>
+        <GridItem>
+          <Button size={{base:"sm", sm:"sm",md:"md",lg:"md"}}>Abs</Button>
+        </GridItem>
+        <GridItem>
+          <Button size={{base:"sm", sm:"sm",md:"md",lg:"md"}}>Lower Legs</Button>
+        </GridItem>
+        <GridItem colSpan={2} position="relative">
+          <Button position="absolute" bottom="0px" size={{base:"sm", sm:"sm",md:"md",lg:"md"}}>Cardio</Button>
+        </GridItem>
+        <GridItem>
+          <Button size={{base:"sm", sm:"sm",md:"md",lg:"md"}}>Upper Legs</Button>
+        </GridItem>
+      </Grid>
+      <Image
+        className={styles.muscleimg}
+        src="https://i.ibb.co/7J6mGV1/stock-photo-d-rendering-muscle-galloping-body-builder-59708131-transformed.png"
+        alt="musclegroup"
+      />
+      <Center className={styles.singlebtn} mt="20px">
+        <Button size={{base:"sm", sm:"sm",md:"md",lg:"md"}} textAlign="center" color="rgb(88, 95, 105)">View All Excercises</Button>
+      </Center>
+    </Box>
+  );
+};
+
+export default MuscleGroup;
