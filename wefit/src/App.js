cc902ce0516cc3bfeb0d34b6388636a9fad7b69a
Vaibhav Kumar Mougha <tunu1994@gmail.com>
2022-12-15 21:46:32 +0530

    added day-3 work by Vaibhav

--- wefit/src/App.js
+++ wefit/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Home from './Pages/Home';
 import AllRoutes from './Routes/AllRoutes';
 
 function App() {
   return (
-    // <AllRoutes/>
-    <Home />
+    <AllRoutes/>
   );
 }
 
