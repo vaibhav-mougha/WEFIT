bb93aca407932aab7ef596bc56323af29b9f0f0e
Shivamt2107 <shivam.gote2107@gmail.com>
2022-12-18 00:44:01 +0530

    added day-4 work by shivam gote

---
+++ wefit/src/Components/Blog/Blog-Pagination/Pagination.jsx
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled";
+import React from "react";
+import ReactPaginate from "react-paginate";
+
+
+
+const Pagination = ({ onSwitchPage, totalPages }) => {
+  const handleClick = async (data) => {
+    onSwitchPage(data.selected + 1);
+  };
+  return (
+    <div>
+      <ReactPaginate
+        previousLabel="← Previous Page"
+        nextLabel="Next Page →"
+        breakLabel="..."
+        pageCount={Math.floor(totalPages / 10)}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handleClick}
+        containerClassName="paginationBlog"
+        pageLinkClassName="page-num"
+        previousLinkClassName="page-num"
+        nextLinkClassName="page-num"
+        activeLinkClassName="active"
+      />
+    </div>
+  );
+};
+
+export default Pagination;
