a4bf058c1a37ec55abf4606145a67175f7ec8127
subhamukherjee321 <subhamukherjee321@gmail.com>
2022-12-17 15:39:34 +0530

    redux function modify

--- wefit/src/Redux/Login/login.reducer.js
+++ wefit/src/Redux/Login/login.reducer.js
@@ -1,89 +1,113 @@
 import {
-    ADMIN_LOGIN_ERROR,
-    ADMIN_LOGIN_FAILED,
-    ADMIN_LOGIN_SUCCESS,
-    USER_LOGIN_ERROR,
-    USER_LOGIN_FAILED,
-    USER_LOGIN_SUCCESS
-  } from "./login.actionTypes";
-  
-  const initState = {
-    user: {
-      userStatus: false,
-      userName: "",
-      failed: false,
-      isError: false
-    },
-    admin: {
-      adminStatus: false,
-      adminName: "",
-      failed: false,
-      isError: false
+  ADMIN_LOGIN_ERROR,
+  ADMIN_LOGIN_FAILED,
+  ADMIN_LOGIN_SUCCESS,
+  LOGOUT,
+  USER_LOGIN_ERROR,
+  USER_LOGIN_FAILED,
+  USER_LOGIN_SUCCESS,
+} from "./login.actionTypes";
+
+const loginUserName = localStorage.getItem("loginUserName") || "";
+const loginAdminName = localStorage.getItem("loginAdminName") || "";
+
+const initState = {
+  user: {
+    userStatus: !!loginUserName,
+    userName: loginUserName,
+    failed: false,
+    isError: false,
+  },
+  admin: {
+    adminStatus: !!loginAdminName,
+    adminName: loginAdminName,
+    failed: false,
+    isError: false,
+  },
+};
+
+const loginReducer = (state = initState, { type, payload }) => {
+  switch (type) {
+    case USER_LOGIN_SUCCESS: {
+      localStorage.setItem("loginUserName", payload);
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          userStatus: true,
+          userName: payload,
+        },
+      };
     }
-  };
-  
-  const loginReducer = (state = initState, { type, payload }) => {
-    switch (type) {
-      case USER_LOGIN_SUCCESS: {
-        return {
-          ...state,
-          user: {
-            ...state.user,
-            userStatus: true,
-            userName: payload
-          }
-        };
-      }
-      case USER_LOGIN_FAILED: {
-        return {
-          ...state,
-          user: {
-            ...state.user,
-            failed: true
-          }
-        };
-      }
-      case USER_LOGIN_ERROR: {
-        return {
-          ...state,
-          user: {
-            ...state.user,
-            isError: true
-          }
-        }
-      }
-      case ADMIN_LOGIN_SUCCESS: {
-        return {
-          ...state,
-          admin: {
-            ...state.admin,
-            adminStatus: true,
-            adminName: payload
-          }
-        };
-      }
-      case ADMIN_LOGIN_FAILED: {
-        return {
-          ...state,
-          admin: {
-            ...state.admin,
-            failed: true
-          }
-        };
-      }
-      case ADMIN_LOGIN_ERROR: {
-        return {
-          ...state,
-          admin: {
-            ...state.admin,
-            isError: true
-          }
-        };
-      }
-      default:
-        return state;
+    case USER_LOGIN_FAILED: {
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          failed: true,
+        },
+      };
     }
-  };
-  
-  export default loginReducer;
-  
+    case USER_LOGIN_ERROR: {
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          isError: true,
+        },
+      };
+    }
+    case ADMIN_LOGIN_SUCCESS: {
+      localStorage.setItem("loginAdminName", payload);
+      return {
+        ...state,
+        admin: {
+          ...state.admin,
+          adminStatus: true,
+          adminName: payload,
+        },
+      };
+    }
+    case ADMIN_LOGIN_FAILED: {
+      return {
+        ...state,
+        admin: {
+          ...state.admin,
+          failed: true,
+        },
+      };
+    }
+    case ADMIN_LOGIN_ERROR: {
+      return {
+        ...state,
+        admin: {
+          ...state.admin,
+          isError: true,
+        },
+      };
+    }
+    case LOGOUT: {
+      localStorage.removeItem("loginAdminName");
+      localStorage.removeItem("loginUserName");
+      return {
+        ...state,
+        user: {
+          userStatus: false,
+          userName: "",
+          failed: false,
+          isError: false,
+        },
+        admin: {
+          adminStatus: false,
+          adminName: "",
+          failed: false,
+          isError: false,
+        },
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default loginReducer;
